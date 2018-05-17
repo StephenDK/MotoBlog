35987d734f34f9f5dca0d2905e11a95927569862
StephenDK <stephendklein08@gmail.com>
2018-05-17 15:19:13 -0700

    Database connection

--- server.js
+++ server.js
@@ -5,14 +5,27 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 // ==============================================
 
-
-
-
-
 // Express instance 
 const app = express();
 
+// Database Connection
+// ==============================================
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost/motoblog";
 
+mongoose.connect(MONGO_URI);
+mongoose.connection
+.once('open', () => console.log("Connected to the database"))
+.on('error', (error) => {
+    console.log("Warning", error);
+});
+// ==============================================
+
+// Middleware
+// ==============================================
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("client/build");
+// ==============================================
 
 
 

4f3df25426447d9e788890a1c988cadf7ca9ef6c
StephenDK <stephendklein08@gmail.com>
2018-05-26 23:34:13 -0700

    react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './Navbar';
+
+
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="container">
        <Navbar />
       </div>
+      </BrowserRouter>
     );
   }
 }

f8db0e9046210478478f83975f02cc4ea3530bd0
StephenDK <stephendklein08@gmail.com>
2018-06-05 20:14:56 -0700

    request object

--- routes/new-post.js
+++ routes/new-post.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = (app) => {
  app.get('/NewPost', function(req, res) {
+     console.log(req);
      res.sendFile(path.join(__dirname, '../NewPost/NewPost.html'));
  })
 }
